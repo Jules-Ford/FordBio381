6b660ab8dc71d5900b4a647676513473bb638de8
Jules <jwilli14@uvm.edu>
2023-03-29 15:39:31 -0400

    Fix hwk 8, start hwk 9, insert Data file on cockroaches

---
+++ docs/Homework9_Functions.R
@@ -0,0 +1,16 @@
+# Homework 9 Functions File
+# Jules Ford
+# 29 March 2023
+
+####################################
+# FUNCTION:  functionname
+# packages: none
+# purpose:
+# input:
+# output:
+#----------------------------
+ functionname <- function() {
+  
+  message("testing... functionname")
+}
+
