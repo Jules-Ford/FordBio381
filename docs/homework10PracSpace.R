16f159755a6d657b9a8fa37746b5ffde30ad4518
Jules <jwilli14@uvm.edu>
2023-04-19 14:59:56 -0400

    fix hwk 10 prob 3

--- docs/homework10PracSpace.R
+++ docs/homework10PracSpace.R
@@ -2,31 +2,9 @@
 # Jules Ford
 # 17 April 2023
 
-# problem 3
-# Write a function that takes as input two integers representing the number of rows and columns in a matrix. The output is a matrix of these dimensions in which each element is the product of the row number x the column number.
+# 4a 
 
-####################################
-# FUNCTION: matrixMaker
-# packages: none
-# purpose: generate a matrix given its row and column
-# input: 2 integers, reprsenting rows and columns in a matrix
-# output: a matrix with the inputted dimensions, where each element is the product of the given row and column number
-#----------------------------
-matrixMaker <- function(rows, cols) {
-  theMatrix <- matrix(data=NA, nrow=rows, ncol=cols)
-  for (i in 1:nrow(theMatrix)) {
-    
-  #  m[i,] <-  need to figure out how to pull the row and column number
-  }
-  return(theMatrix)
-} # this is a great start. Now I just need to fill the rows and columns with the product of the row and column
-
-test <- matrixMaker(3,4)
-
-
-m <- matrix(data=NA, nrow=3, ncol=6)
-m[3,4] <- 5 # so this is how you assign a variable
-m[,]
+#Simulate a dataset with 3 groups of data, each group drawn from a distribution with a different mean. The final data frame should have 1 column for group and 1 column for the response variable.
 
 
 
